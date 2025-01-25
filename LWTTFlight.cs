b5aa42521704cf8be832bfed9586563d6c5410e7
clacla17 <clafairy07@gmail.com>
2025-01-26 04:22:47 +0800

    refined LWTTFlight.cs
    constructors

--- LWTTFlight.cs
+++ LWTTFlight.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +9,15 @@ namespace PRG2_Assignment
     public class LWTTFlight : Flight
     {
         public double RequestFee { get; set; }
-
+        public LWTTFlight() : base() { }
+        public LWTTFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status = "On Time") : base(flightNumber, origin, destination, expectedTime, status) { }
         public override double CalculateFees()
         {
             return RequestFee;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
         }
     }
 }
