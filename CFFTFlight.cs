a2b70cbe0896204bfe9374cb6bf90c21bb271a18
clacla17 <clafairy07@gmail.com>
2025-01-26 04:18:27 +0800

    refined CFFTFlight.cs
    constructors again

--- CFFTFlight.cs
+++ CFFTFlight.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +9,15 @@ namespace PRG2_Assignment
     public class CFFTFlight : Flight
     {
         public double RequestFee { get; set; }
-
+        public CFFTFlight() : base() { }
+        public CFFTFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status = "On Time") : base(flightNumber, origin, destination, expectedTime, status) { }
         public override double CalculateFees()
         {
             return RequestFee;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
         }
     }
 }
