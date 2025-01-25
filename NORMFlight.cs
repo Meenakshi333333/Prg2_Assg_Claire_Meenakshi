d9d7d3e09f09f70feeaeec215ba6bb51cd945607
clacla17 <clafairy07@gmail.com>
2025-01-26 04:23:51 +0800

    refined NORMFlight.cs
    constructors agn

--- NORMFlight.cs
+++ NORMFlight.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +8,17 @@ namespace PRG2_Assignment
 {
     public class NORMFlight : Flight
     {
+        //constructors
+        public NORMFlight():base() { }
+        public NORMFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status = "On Time") : base(flightNumber, origin, destination, expectedTime, status) { }
         public override double CalculateFees()
         {
-            return 100.0; // Example fee calculation
+            double fees = 0;
+            return fees;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
         }
     }
 }
