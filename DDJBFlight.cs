7288b37d07d1fc01cfde36f5e57e0e371769d4ca
clacla17 <clafairy07@gmail.com>
2025-02-01 01:10:29 +0800

    fixed tostring method

--- DDJBFlight.cs
+++ DDJBFlight.cs
@@ -1,3 +1,9 @@
+//==========================================================
+// Student Number	: S10267951B
+// Student Name	: Claire Chan
+// Partner Name	: Senthilnathan Meeankshi
+//==========================================================
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,7 +23,7 @@ namespace PRG2_Assignment
         }
         public override string ToString()
         {
-            return base.ToString();
+            return base.ToString() + "\nSpecial Request Code: DDJB";
         }
     }
 }
